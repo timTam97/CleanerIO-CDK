a19f02b9f23c8f9aadff6810f614c952478a1b9a
Timothy Samraj <tsam0005@student.monash.edu>
2021-04-17 16:02:46 +1000

    View logs function working

--- lib/src/viewlogs/app.py
+++ lib/src/viewlogs/app.py
@@ -1,52 +1,35 @@
 """
-Lambda authorizer for when clients try to connect
-to the websocket. We check that their key is present
-in the DB of generated keys.
+todo lol
 """
+import json
 import os
+from decimal import Decimal
 
 import boto3
 from boto3.dynamodb.conditions import Key
 
-key_table = boto3.resource("dynamodb").Table(os.environ.get("TABLE_NAME"))
+logs_table = boto3.resource("dynamodb").Table(os.environ.get("TABLE_NAME"))
 
 
 def handler(event, context):
     print(event)
-    method = event["methodArn"]
-    auth_token = event["headers"].get("auth")
-    if auth_token is None:
-        return deny(method)
-    res = key_table.query(KeyConditionExpression=Key("key").eq(auth_token))
-    if len(res["Items"]) == 0:
-        return deny(method)
-    return allow(method)
-
-
-def allow(resource):
-    return generate_policy("me", "Allow", resource)
-
-
-def deny(resource):
-    return generate_policy("me", "Deny", resource)
-
-
-def generate_policy(principal_id, effect, resource):
-    """
-    Generates an IAM policy.
-    """
+    payload = json.loads(event["body"])
+    print(payload)
+    res = logs_table.get_item(
+        Key={"trainID": payload["trainID"], "startTime": payload["startTime"]}
+    )
+    data = res["Item"]
+    print(data)
+    # mini rant - why does boto3 return numbers as decimals??????????
+    # have to go and turn everything into an integer now
+    for key, val in data.items():
+        if isinstance(val, Decimal):
+            data[key] = int(val)
+        elif isinstance(val, list) and all(isinstance(x, Decimal) for x in val):
+            data[key] = list(map(int, data[key]))
+    print(res)
     return {
-        "principalId": principal_id,
-        "policyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Action": "execute-api:Invoke",
-                    "Effect": effect,
-                    "Resource": resource,
-                }
-            ],
-        },
-        "context": {"stringKey": "value", "numberKey": "1", "booleanKey": "true"},
-        "usageIdentifierKey": "{api-key}",
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "body": json.dumps(res["Item"]),
     }
